2e59b6958d1bf54229ee9108129ffdbf3a4c1b36
Boris Bügling <boris@icculus.org>
2015-12-24 11:47:25 +0100

    Use GH flavoured MD extensions for web view

--- Code/MDWebViewController.swift
+++ Code/MDWebViewController.swift
@@ -22,10 +22,13 @@ class MDWebViewController: UIViewController {
         let webView = WKWebView(frame: view.bounds)
         view.addSubview(webView)
 
+        webView.scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: tabBarController?.tabBar.frame.size.height ?? 0, right: 0)
+        webView.scrollView.scrollIndicatorInsets = webView.scrollView.contentInset
+
         client.fetchEntry("4bJdF7zhwkwcWq202gmkuM").1.next { (entry) in
             if let markdown = entry.fields["body"] as? String {
                 do {
-                    let html = try MMMarkdown.HTMLStringWithMarkdown(markdown)
+                    let html = try MMMarkdown.HTMLStringWithMarkdown(markdown, extensions: MMMarkdownExtensions.GitHubFlavored)
                     webView.loadHTMLString(html, baseURL: nil)
                 } catch let error {
                     showError(error, self)
