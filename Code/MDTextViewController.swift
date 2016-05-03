b4a571b1021b767d976a21f436da445fc2548a8b
Boris Bügling <boris@icculus.org>
2016-05-03 15:53:50 +0200

    Actually implement the CocoaMarkdown example
    We should eventually do some more here, like showing how folks can
    handle images, links, etc -- but this at least serves as a pointer in
    the right direction.

--- Code/MDTextViewController.swift
+++ Code/MDTextViewController.swift
@@ -10,6 +10,8 @@ import CocoaMarkdown
 import UIKit
 
 class MDTextViewController: UIViewController {
+    var entryId = "4bJdF7zhwkwcWq202gmkuM"
+
     convenience init() {
         self.init(nibName: nil, bundle: nil)
 
@@ -18,5 +20,23 @@ class MDTextViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let textView = UITextView(frame: view.bounds)
+        textView.editable = false
+        view.addSubview(textView)
+
+        client.fetchEntry(entryId).1.next { (entry) in
+            if let markdown = entry.fields["body"] as? String {
+                let data = markdown.dataUsingEncoding(NSUTF8StringEncoding)
+                let document = CMDocument(data: data, options: [])
+                let renderer = CMAttributedStringRenderer(document: document, attributes: CMTextAttributes())
+
+                dispatch_async(dispatch_get_main_queue()) {
+                    textView.attributedText = renderer.render()
+                }
+            }
+        }.error { (error) in
+            showError(error, self)
+        }
     }
 }
